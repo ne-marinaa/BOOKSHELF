599395792ed673aff543ffea5d31974102e61d92
ne-marinaa <149939065+ne-marinaa@users.noreply.github.com>
2023-12-02 18:53:41 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	$(".hamburger").click(function(){
+		$(this).toggleClass("is-active");
+
+		if($(this).hasClass('is-active')){
+			$('.mnu_top').slideDown(300);
+		}else{
+			$('.mnu_top').slideUp(300);
+		}
+	});
+
+});
